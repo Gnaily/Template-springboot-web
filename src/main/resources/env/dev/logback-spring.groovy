3c299cea38d01585f6ed8be64f09f2584f24b313
Gnaily <y1iagx@163.com>
2020-01-30 01:43:41 +0800

    增加Logback配置

---
+++ src/main/resources/env/dev/logback-spring.groovy
@@ -0,0 +1,64 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.filter.LevelFilter
+import ch.qos.logback.core.ConsoleAppender
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
+import static ch.qos.logback.core.spi.FilterReply.ACCEPT
+
+//see  http://logback.qos.ch/manual/groovy.html
+//没分钟扫描一次配置
+scan()
+
+def log_home = System.getProperty("LOG_HOME");
+
+if(log_home == null){
+    if(System.getProperty("os.name").contains("Windows")){
+        log_home = "d:/logs";
+    }else{
+        log_home = "/data/www/logs/xxsystem/";
+    }
+}
+
+println "log home ==> ${log_home}"
+
+/***控制台日志追加器配置***/
+appender("CONSOLE", ConsoleAppender) {
+    /**编码器**/
+    encoder(PatternLayoutEncoder) {
+        //日志时间-日志级别-线程-类-代码行
+        pattern = "%d{yyyy/MM/dd-HH:mm:ss} %-5level [%thread] %class{5}:%line>>%msg%n"
+    }
+}
+def byMinutes  = timestamp("yyyy-MM-dd'T'HH-mm")
+//文件日志配置
+appender("FILE", RollingFileAppender) {
+    append = true;
+    //给新产生的log文件命
+    file = "${log_home}/${byMinutes}.log";
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger{80} - %msg%n"
+    }
+
+    filter(LevelFilter) {
+        level = INFO
+        onMatch = ACCEPT
+        //onMismatch = DENY
+    }
+
+    //移动文件以及对文件改名
+    rollingPolicy(TimeBasedRollingPolicy){
+        //分隔出来文件的命名模式
+        fileNamePattern = "${log_home}/%d{yyyy-MM-dd}.log";
+        //保留时间，超过这个时间的日志文件会自动删除
+        maxHistory = 15;
+    }
+
+    // 日志文件达到指定大小触发滚动
+    triggeringPolicy(SizeBasedTriggeringPolicy){
+        maxFileSize = "20mb";
+    }
+}
+
+root(DEBUG, ["CONSOLE", "FILE"])
+logger("org.springframework", INFO)
